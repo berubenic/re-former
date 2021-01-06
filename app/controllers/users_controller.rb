28d321ab98e4c0b0a63e369f4bc9feb5a9bd22d3
Nicholas <berubenic@gmail.com>
2021-01-06 15:25:34 -0500

    form with only html, saves but when missing something the entered data is not reloaded with the form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
 end
